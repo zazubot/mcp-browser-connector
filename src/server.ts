abb878427167d4ee09c40906e473ab68dae041ce
Ahmed Khaled <ahmedkhaled4d@gmail.com>
2026-06-28 22:51:51 +0300

    init modelcontextprotocol

---
+++ src/server.ts
@@ -0,0 +1,92 @@
+import { Server } from "@modelcontextprotocol/sdk/server/index.js";
+import {
+  CallToolRequestSchema,
+  ListResourcesRequestSchema,
+  ListToolsRequestSchema,
+  ReadResourceRequestSchema,
+} from "@modelcontextprotocol/sdk/types.js";
+
+import { Context } from "@/context";
+import type { Resource } from "@/resources/resource";
+import type { Tool } from "@/tools/tool";
+import { createWebSocketServer } from "@/ws";
+
+type Options = {
+  name: string;
+  version: string;
+  tools: Tool[];
+  resources: Resource[];
+};
+
+export async function createServerWithTools(options: Options): Promise<Server> {
+  const { name, version, tools, resources } = options;
+  const context = new Context();
+  const server = new Server(
+    { name, version },
+    {
+      capabilities: {
+        tools: {},
+        resources: {},
+      },
+    },
+  );
+
+  const wss = await createWebSocketServer();
+  wss.on("connection", (websocket) => {
+    // Close any existing connections
+    if (context.hasWs()) {
+      context.ws.close();
+    }
+    context.ws = websocket;
+  });
+
+  server.setRequestHandler(ListToolsRequestSchema, async () => {
+    return { tools: tools.map((tool) => tool.schema) };
+  });
+
+  server.setRequestHandler(ListResourcesRequestSchema, async () => {
+    return { resources: resources.map((resource) => resource.schema) };
+  });
+
+  server.setRequestHandler(CallToolRequestSchema, async (request) => {
+    const tool = tools.find((tool) => tool.schema.name === request.params.name);
+    if (!tool) {
+      return {
+        content: [
+          { type: "text", text: `Tool "${request.params.name}" not found` },
+        ],
+        isError: true,
+      };
+    }
+
+    try {
+      const result = await tool.handle(context, request.params.arguments);
+      return result;
+    } catch (error) {
+      return {
+        content: [{ type: "text", text: String(error) }],
+        isError: true,
+      };
+    }
+  });
+
+  server.setRequestHandler(ReadResourceRequestSchema, async (request) => {
+    const resource = resources.find(
+      (resource) => resource.schema.uri === request.params.uri,
+    );
+    if (!resource) {
+      return { contents: [] };
+    }
+
+    const contents = await resource.read(context, request.params.uri);
+    return { contents };
+  });
+
+  server.close = async () => {
+    await server.close();
+    await wss.close();
+    await context.close();
+  };
+
+  return server;
+}
